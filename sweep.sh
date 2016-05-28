cf416bfba765619e377f66d302291f6cb6ef89a2
Raejoon Jung <raejoon@stanford.edu>
2016-05-28 04:11:20 -0700

    changed sweep script to make separate testcase directories

--- sweep.sh
+++ sweep.sh
@@ -4,18 +4,20 @@ set -e
 
 for TOPO in MESH TORUS;
 do
-    sed -i "s/^parameter topology.*/parameter topology = \`TOPOLOGY_${TOPO}/" "./verif/mesh_generate/parameters.v"
-
     for SIZE in 2 4 6;
     do
-        sed -i "s/^parameter topo_width.*/parameter topo_width = ${SIZE};/" "./verif/mesh_generate/parameters.v"
-        sed -i "s/^parameter topo_height.*/parameter topo_height = ${SIZE};/" "./verif/mesh_generate/parameters.v"
-
         for RATE in 12 25 37 50;
         do
-            sed -i "s/^parameter packet_rate.*/parameter packet_rate = ${RATE};/" "./verif/mesh_generate/parameters.v"
+            DIR=${TOPO}_${SIZE}_${RATE}
+            cp -rf ./verif/mesh_generate ./verif/${DIR}
 
-            make TESTCASE=${TOPO}_${SIZE}_${RATE}
+            sed -i "s/^parameter topology.*/parameter topology = \`TOPOLOGY_${TOPO}/" "./verif/${DIR}/parameters.v"
+            sed -i "s/^parameter topo_width.*/parameter topo_width = ${SIZE};/" "./verif/${DIR}/parameters.v"
+            sed -i "s/^parameter topo_height.*/parameter topo_height = ${SIZE};/" "./verif/${DIR}/parameters.v"
+
+            sed -i "s/^parameter packet_rate.*/parameter packet_rate =${RATE};/" "./verif/${DIR}/parameters.v"
+
+            make TESTCASE=${DIR}
         done
     done
 done
@@ -24,13 +26,15 @@ done
 # for flatten butterfly
 for SIZE in 2 4 6;
     do
-        sed -i "s/^parameter topo_width.*/parameter topo_width = ${SIZE};/" "./verif/fbfly_generate/parameters.v"
-        sed -i "s/^parameter topo_height.*/parameter topo_height = ${SIZE};/" "./verif/fbfly_generate/parameters.v"
-
         for RATE in 12 25 37 50;
         do
-            sed -i "s/^parameter packet_rate.*/parameter packet_rate = ${RATE};/" "./verif/fbfly_generate/parameters.v"
+            DIR=FBFLY_${SIZE}_${RATE}
+            cp -rf ./verif/fbfly_generate ./verif/${DIR}
 
-            make TESTCASE=FBFLY_${SIZE}_${RATE}
+            sed -i "s/^parameter topo_width.*/parameter topo_width = ${SIZE};/" "./verif/${DIR}/parameters.v"
+            sed -i "s/^parameter topo_height.*/parameter topo_height = ${SIZE};/" "./verif/${DIR}/parameters.v"
+
+            sed -i "s/^parameter packet_rate.*/parameter packet_rate =${RATE};/" "./verif/${DIR}/parameters.v"
+            make TESTCASE=FBFLY_${DIR}
         done
 done
